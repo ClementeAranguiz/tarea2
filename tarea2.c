1574685eb0cf010b15cd5fac0961baa8ce077f7c
CLEMENTE ARÁNGUIZ VERGARA (Alumno) <clemente.aranguiz@usm.cl>
2020-07-26 18:51:34 -0400

    arreglé la weaa del insertionsort

--- tarea2.c
+++ tarea2.c
@@ -3,22 +3,18 @@
 #include <conio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <float.h>
 
-void insertionsort(int *lista){
-    int i=0;
-    while(lista[i]!=-1){
-        
-        int current=lista[i];
-        printf("%d\n",lista[i]);
-        int prev=current-1;
-        while(prev>=0 && lista[prev]>current){
-            printf("%d\n",lista[i]);
-            lista[prev+1]=lista[prev];
-            printf("%d\n",lista[i]);
+void insertionsort(int **lista){
+    int i=1;
+    while((*lista)[i]!=-1){
+        int current=(*lista)[i];
+        int prev=i-1;
+        while(prev>=0 && (*lista)[prev]>current){
+            (*lista)[prev+1]=(*lista)[prev];
             prev--;
         }
-        lista[prev+1]=current; 
-        //printf("%d\n",lista[current]);
+        (*lista)[prev+1]=current; 
         i++;
     }
     
@@ -36,12 +32,12 @@ int main(){
         array[contador]=atoi(current);
         contador++;
     }
+    fclose(pfile);
     array=realloc(array, sizeof(int)*(contador+1));
     array[contador]=-1;
-
-    fclose(pfile);
-    insertionsort(array);
-    //printf("%d",array[0]);
+    printf("%d\n",array[149]);
+    insertionsort(&array);
+    printf("%d",array[149]);
     free(array);
     return 0;
 } 
